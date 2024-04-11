82800968becb1e801a924e010e6cffba158a6d3b
Aleksandra Szlacheta <aleksandra.szlacheta@gmail.com>
2024-04-11 19:14:19 +0200

    feat(project-init) Enable internationalization

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { ProjectWizard } from './pages/projects-wizard/ProjectWizard.tsx';
 import './App.css';
+import './i18n.config.ts';
 
 export const App = () => {
 
-  return (
-    <>
+    // const { mutate } = useAddProject();
+    //
+    // useEffect(() => {
+    //     const project = { name: '', description: 'dummy', dataRange: [1, 2], areaOfInterest: {} };
+    //     console.log(mutate({ data: project }));
+    //     // add({ name: '', description: 'dummy', dataRange: [1, 2], areaOfInterest: {} })
+    //     //     .then(() => {
+    //     //         console.log('success');
+    //     //     }).catch((error) => {
+    //     //     console.log('error', error);
+    //     // });
+    // }, [mutate]);
 
-    </>
-  );
+
+    return (
+        <>
+            <ProjectWizard activeStep={DEFAULT_ACTIVE_STEP_INDEX}/>
+        </>
+    );
 };
